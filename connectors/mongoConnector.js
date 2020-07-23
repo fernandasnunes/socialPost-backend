0a6826513179b0a3664fcf9769fc9b13d9456b1c
Fernanda Nunes <fernanda.nunes@ibm.com>
2020-07-23 17:17:47 -0300

    adjust connection

--- connectors/mongoConnector.js
+++ connectors/mongoConnector.js
@@ -1,48 +1,16 @@
 const mongoose = require('mongoose')
 
-/* -------------------mongoConnect----------------------
-Responsible for create a mongo connection based on its
-credentials
-------------------------------------------------------*/
-
-
-// async function mongoConnect() {
-//     try {
-//         try {
-//             mongoose.connect(process.env.uri, {useNewUrlParser: true, useUnifiedTopology: true}, () =>
-//             console.log("connected"));    
-//             }catch (error) { 
-//             console.log("could not connect");    
-//             }
-
-//     } catch (error) {
-//         throw (error)
-//     }
-// }
-
-// module.exports = {
-//     mongoConnect: mongoConnect
-// }
-
-
-/* -------------------mongoConnect----------------------
-Responsible for create a mongo connection based on its
-credentials
-------------------------------------------------------*/
 async function mongoConnect() {
     try {
-        mongoose.connect(process.env.uri, {
-                                    
-            useNewUrlParser: true,
-            
-            useUnifiedTopology: true,
-        }).catch(err => {
-            throw new Error(err)
-        })
 
-        const db = mongoose.connection
-        db.on('error', console.error.bind(console, 'connection error:'))
-        db.once('open', console.log.bind(console, 'Mongo ready'))
+    mongoose.connect('mongodb+srv://dbUser:8DasQNXtih054Df1@cluster0-bi5e4.mongodb.net/admin?retryWrites=true&w=majority' , {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+const db = mongoose.connection
+db.on('error', console.error.bind(console, 'connection error:'))
+db.once('open', console.log.bind(console, 'Mongo ready'))
 
     } catch (error) {
         throw (error)
